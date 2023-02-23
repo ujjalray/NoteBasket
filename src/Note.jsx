109e9dc69b1d46b0549dcc72a34aacdecdf80a7e
ujjal ray <codingujjal@gmail.com>
2023-02-23 21:56:21 +0530

    All file update

---
+++ src/Note.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Note = (props) => {
+
+   const handleClick=()=>
+   {
+       props.onDelete(props.id);
+   }
+
+   return (
+
+    <div className="note">
+      <h1>{props.title}</h1>
+      <p>{props.content}</p>
+      <button onClick={handleClick}>DELETE</button>
+    </div>
+    
+  );
+};
+
+export default Note ;
+
+
+
+
+
+
+
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+
+           <h1>NoteBasket</h1>
+           
+    </header>
+  );
+}
+
+export default Header;
